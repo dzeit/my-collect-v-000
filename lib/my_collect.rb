8940e83bdf1a62fed5dd079a85d409754b0d13d5
David Zeit <dzeit216@gmail.com>
2018-08-29 20:19:28 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 
 def my_collect(array)
-  counter = 0
+  element = 0
   new_array = []
-  while counter < array.length
-  yield(array[counter])
-  counter += 1
+  while element < array.length
+  yield(array[element])
+  element += 1
   end
   new_array
 end

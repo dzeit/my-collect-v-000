89032913e885a8821934b9ab654e1103ca623e8a
David Zeit <dzeit216@gmail.com>
2018-08-29 20:24:12 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
   element = 0
   new_array = []
   while element < array.length
-  new_array << yield(array[element])
+  new_array << yield(array[element]) # yield not only pauses but also passes the formula block to modify each element.
   element += 1
   end
   new_array
